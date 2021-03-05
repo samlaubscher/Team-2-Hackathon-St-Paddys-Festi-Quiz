9e5403570ee65b7c14d24f019ce91a84a5a1d13f
Sam Laubscher <hakaewt@protonmail.com>
2021-03-05 09:56:23 +0000

    Update script paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ function hideNamePanel() {
     document.getElementById("main-landing-container").classList.toggle("hidden");
     document.getElementById("enter-name-container").classList.toggle("hidden");
 }
+
+/* questions scripts */ 
+
+function nextQuestion(number){
+    let questionNumber = parseInt(number)
+    let currentQuestionId = `q${number}`
+    let nextQuestionId = `q${questionNumber + 1}`
+    if (questionNumber == 4){
+        window.location.replace("http://www.google.co.uk")
+    } else {
+        $(`#${currentQuestionId}`).append(`            
+                <button type="button" data-bs-target="#questionsCarousel" data-bs-slide-to="${questionNumber + 1}"
+                aria-current="true" aria-label="Slide 1">Next</button>`)
+    }
+}
